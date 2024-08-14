0be4f449bd767dbfce38ec5b218fc83daf45f37f
luca_di_fiori <lucadifiori@hotmail.it>
2024-08-14 14:52:07 +0200

    fix

--- ex00/magaphone.cpp
+++ ex00/magaphone.cpp
@@ -21,7 +21,11 @@ int main(int argc, char **argv)
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
+			{
 				std::cout << (char)std::toupper(argv[i][j]);
+			}
+			if (i < argc - 1)
+				std::cout << " ";
 		}
 		std::cout << std::endl;
 	}
