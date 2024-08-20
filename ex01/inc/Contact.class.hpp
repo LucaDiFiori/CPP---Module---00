f982774973e87d6b479c3d276e52e384280a17c4
luca_di_fiori <lucadifiori@hotmail.it>
2024-08-20 11:53:33 +0200

    Aggiunto Contact .hpp/cpp e Makefile

---
+++ ex01/inc/Contact.class.hpp
@@ -0,0 +1,35 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickName;
+		std::string _phoneNumber;
+		std::string _darkSecret;
+	
+	public:
+		//constructor
+		Contact();
+
+		//getters. "Const" at the end of the function means that the function is not allowed to modify the object.
+		std::string getFisrtName() const;
+		std::string getLastName() const;
+		std::string getNickName() const;
+		std::string getPhoneNumber() const;
+		std::string getDarkSecret() const;
+
+		//setters
+		void setFirstName(std::string firstName);
+		void setLastName(std::string lastName);
+		void setNickName(std::string nickName);
+		void setPhoneNumber(std::string phoneNumber);
+		void setDarkSecret(std::string darkSecret);
+};
+
+#endif
